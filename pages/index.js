aaa9344135f64e10c68ab3d58c922f07952ecf87
scottie-schneider <scott@laclave.co>
2021-03-25 10:35:19 -0500

    first commit

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,12 @@
-import Head from 'next/head'
+import Head from "next/head";
 
+// 1. Add the html
+// 2. layout the things
+// 3. spacing
+// 4. box properties
+// 5. typography
+// 6. fun element
+// 7. responsive
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
@@ -10,14 +17,14 @@ export default function Home() {
 
       <main className="flex flex-col items-center justify-center flex-1 px-20 text-center">
         <h1 className="text-6xl font-bold">
-          Welcome to{' '}
+          Welcome to{" "}
           <a className="text-blue-600" href="https://nextjs.org">
             Next.js!
           </a>
         </h1>
 
         <p className="mt-3 text-2xl">
-          Get started by editing{' '}
+          Get started by editing{" "}
           <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
             pages/index.js
           </code>
@@ -73,10 +80,10 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Powered by{" "}
           <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
         </a>
       </footer>
     </div>
-  )
+  );
 }
